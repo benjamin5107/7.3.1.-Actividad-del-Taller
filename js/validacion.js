cdbbbe214f6bbff677f4d4d0f60c1408ed7421d7
santibarcelo55 <santibarcelo55@gmail.com>
2025-08-13 13:59:10 -0300

    Agregar validaciones y alertas en el formulario

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+const regBtn = document.getElementById('regBtn');
+
+regBtn.addEventListener('click', function() {
+    const password1 = document.getElementById('password1').value;
+
+    if (password1.length < 6) {
+       
+        showAlertError();
+    } else {
+       
+        showAlertSuccess();
+    }
+});
